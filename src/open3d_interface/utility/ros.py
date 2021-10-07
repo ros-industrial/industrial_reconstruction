92090c67fb68db5db9be7892fd8a6fad7eba13ac
marrts <41449746+marrts@users.noreply.github.com>
2021-10-07 16:12:18 -0500

    Added ability to integrate live incoming camera data (#2)
    * Added ability to integrate live incoming camera data or start integration immediately
    
    * Ability to publish mesh while running live
    
    * Cleaned up issues from testing
    
    Co-authored-by: Tyler Marr <tyler.marr@swri.org>

--- src/open3d_interface/utility/ros.py
+++ src/open3d_interface/utility/ros.py
@@ -4,6 +4,10 @@
 
 import json
 import open3d as o3d
+import numpy as np
+from geometry_msgs.msg import Point, Vector3, Pose
+from visualization_msgs.msg import Marker
+from std_msgs.msg import ColorRGBA
 
 def save_camera_info_intrinsic_as_json(filename, camera_info_msg):
     with open(filename, 'w') as outfile:
@@ -25,3 +29,35 @@ def save_camera_info_intrinsic_as_json(filename, camera_info_msg):
 def getIntrinsicsFromMsg(camera_info_msg):
   return o3d.camera.PinholeCameraIntrinsic(camera_info_msg.width, camera_info_msg.height, camera_info_msg.K[0], camera_info_msg.K[4], camera_info_msg.K[2], camera_info_msg.K[5])
 
+def meshToRos(mesh):
+    triangles = np.asarray(mesh.triangles)
+    vertices = np.asarray(mesh.vertices)
+    vertex_colors = np.asarray(mesh.vertex_colors)
+    out_msg = Marker()
+    out_msg.type = out_msg.TRIANGLE_LIST
+    out_msg.action = out_msg.ADD
+    out_msg.id = 1
+    out_msg.scale.x = 1.0
+    out_msg.scale.y = 1.0
+    out_msg.scale.z = 1.0
+    out_msg.pose.position.x = 0
+    out_msg.pose.position.y = 0
+    out_msg.pose.position.z = 0
+    out_msg.pose.orientation.w = 1
+    out_msg.pose.orientation.x = 0
+    out_msg.pose.orientation.y = 0
+    out_msg.pose.orientation.z = 0
+    for triangle in triangles:
+        for vertex_index in triangle:
+            curr_point = Point()
+            curr_point.x = vertices[vertex_index][0]
+            curr_point.y = vertices[vertex_index][1]
+            curr_point.z = vertices[vertex_index][2]
+            curr_point_color = ColorRGBA()
+            curr_point_color.r = vertex_colors[vertex_index][2]
+            curr_point_color.g = vertex_colors[vertex_index][1]
+            curr_point_color.b = vertex_colors[vertex_index][0]
+            curr_point_color.a = 1
+            out_msg.points.append(curr_point)
+            out_msg.colors.append(curr_point_color)
+    return out_msg
--- src/open3d_interface/yak/__init__.py
+++ src/open3d_interface/yak/__init__.py
@@ -18,12 +18,13 @@ from message_filters import ApproximateTimeSynchronizer, Subscriber
 from open3d_interface.utility.file import make_clean_folder, write_pose, read_pose, save_intrinsic_as_json
 from open3d_interface.srv import StartYakReconstruction, StopYakReconstruction
 from open3d_interface.srv import StartYakReconstructionResponse, StopYakReconstructionResponse
-from open3d_interface.utility.ros import getIntrinsicsFromMsg
+from open3d_interface.utility.ros import getIntrinsicsFromMsg, meshToRos
 
 # ROS Image message -> OpenCV2 image converter
 from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
 import cv2
+from visualization_msgs.msg import Marker
 
 bridge = CvBridge()
 
@@ -54,9 +55,16 @@ rgb_poses = []
 prev_pose_rot = np.array([1.0, 0.0, 0.0, 0.0])
 prev_pose_tran = np.array([0.0, 0.0, 0.0])
 
+tsdf_integration_data = deque()
+integration_done = True
+live_integration = False
+mesh_pub = None
+
 record = False
 frame_count = 0
 processed_frame_count = 0
+reconstructed_frame_count = 0
+
 
 def archiveData(path_output):
   global depth_images, color_images, rgb_poses, intrinsics
@@ -81,7 +89,7 @@ def startYakReconstructionCallback(req):
   global record, frame_count, processed_frame_count, relative_frame, tracking_frame
   global color_images, depth_images, rgb_poses, sensor_data, tsdf_volume
   global depth_scale, depth_trunc, convert_rgb_to_intensity
-  global prev_pose_rot, prev_pose_tran, translation_distance, rotational_distance
+  global prev_pose_rot, prev_pose_tran, translation_distance, rotational_distance, live_integration
 
   rospy.loginfo(rospy.get_caller_id() + ": Start Reconstruction")
 
@@ -89,11 +97,13 @@ def startYakReconstructionCallback(req):
   depth_images.clear()
   rgb_poses.clear()
   sensor_data.clear()
+  tsdf_integration_data.clear()
   prev_pose_rot = np.array([1.0, 0.0, 0.0, 0.0])
   prev_pose_tran = np.array([0.0, 0.0, 0.0])
 
   frame_count = 0
   processed_frame_count = 0
+  reconstructed_frame_count = 0
 
   tsdf_volume = o3d.pipelines.integration.ScalableTSDFVolume(
       voxel_length=req.tsdf_params.voxel_length,
@@ -108,23 +118,30 @@ def startYakReconstructionCallback(req):
   translation_distance = req.translation_distance
   rotational_distance = req.rotational_distance
 
+  live_integration = req.live
   record = True
   return StartYakReconstructionResponse(True)
 
 def stopYakReconstructionCallback(req):
   global record, tsdf_volume, depth_images, color_images, rgb_poses, depth_scale, depth_trunc, intrinsics
+  global integration_done, relative_frame
 
   rospy.loginfo(rospy.get_caller_id() + ": Stop Reconstruction")
   record = False
 
-  for s in range(len(color_images)):
-    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color_images[s], depth_images[s], depth_scale, depth_trunc, False)
-    tsdf_volume.integrate(rgbd, intrinsics, np.linalg.inv(rgb_poses[s]))
+  while not integration_done:
+    rospy.sleep(1.0)
 
+  print("Generating mesh")
   mesh = tsdf_volume.extract_triangle_mesh()
   mesh.compute_vertex_normals()
   mesh_filepath = join(req.results_directory, "integrated.ply")
   o3d.io.write_triangle_mesh(mesh_filepath, mesh, False, True)
+  mesh_msg = meshToRos(mesh)
+  mesh_msg.header.stamp = rospy.get_rostime()
+  mesh_msg.header.frame_id = relative_frame
+  mesh_pub.publish(mesh_msg)
+  print("Mesh Generated")
 
   if (req.archive_directory != ""):
     rospy.loginfo(rospy.get_caller_id() + ": Archiving data to " + req.archive_directory)
@@ -138,14 +155,17 @@ def stopYakReconstructionCallback(req):
 def cameraCallback(depth_image_msg, rgb_image_msg):
   global frame_count, processed_frame_count, record, tracking_frame, relative_frame, tf_listener
   global color_images, depth_images, rgb_poses, intrinsics, prev_pose_rot, prev_pose_tran
+  global tsdf_integration_data, live_integration, integration_done, mesh_pub
 
   if record:
     try:
         # Convert your ROS Image message to OpenCV2
+        # TODO: Generalize image type
         cv2_depth_img = bridge.imgmsg_to_cv2(depth_image_msg, "16UC1")
         cv2_rgb_img = bridge.imgmsg_to_cv2(rgb_image_msg, "bgr8")
     except CvBridgeError:
         print(e)
+        return
     else:
         # Get camera intrinsic from camera info
         if frame_count == 0:
@@ -155,13 +175,17 @@ def cameraCallback(depth_image_msg, rgb_image_msg):
         sensor_data.append([o3d.geometry.Image(cv2_depth_img), o3d.geometry.Image(cv2_rgb_img), rgb_image_msg.header.stamp])
         if (frame_count > 30):
           data = sensor_data.popleft()
-          (rgb_t,rgb_r) = tf_listener.lookupTransform(relative_frame, tracking_frame, data[2])
+          try:
+              (rgb_t,rgb_r) = tf_listener.lookupTransform(relative_frame, tracking_frame, data[2])
+          except:
+              return
           rgb_t = np.array(rgb_t)
           rgb_r = np.array(rgb_r)
 
           tran_dist = np.linalg.norm(rgb_t - prev_pose_tran)
           rot_dist = Quaternion.absolute_distance(Quaternion(prev_pose_rot), Quaternion(rgb_r))
 
+          # TODO: Testing if this is a good practice, min jump to accept data
           if (tran_dist > translation_distance) or (rot_dist > rotational_distance):
             prev_pose_tran = rgb_t
             prev_pose_rot = rgb_r
@@ -173,14 +197,47 @@ def cameraCallback(depth_image_msg, rgb_image_msg):
             depth_images.append(data[0])
             color_images.append(data[1])
             rgb_poses.append(rgb_pose)
-
+            if live_integration:
+              integration_done = False
+              rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(data[1], data[0], depth_scale, depth_trunc, False)
+              tsdf_volume.integrate(rgbd, intrinsics, np.linalg.inv(rgb_pose))
+              integration_done = True
+              if processed_frame_count % 50 == 0:
+                  mesh = tsdf_volume.extract_triangle_mesh()
+                  mesh_msg = meshToRos(mesh)
+                  mesh_msg.header.stamp = rospy.get_rostime()
+                  mesh_msg.header.frame_id = relative_frame
+                  mesh_pub.publish(mesh_msg)
+            else:
+              tsdf_integration_data.append([data[0], data[1], rgb_pose])
             processed_frame_count += 1
 
         frame_count += 1
 
+def timerReconstruction(timer):
+  global depth_scale, depth_trunc, convert_rgb_to_intensity
+  global tsdf_integration_data, integration_done, processed_frame_count, reconstructed_frame_count
+  integrating_queue = False
+  while len(tsdf_integration_data) > 0:
+      integrating_queue = True
+      print("Integrating,", len(tsdf_integration_data), "images left to integrate")
+      integration_done = False
+      data = tsdf_integration_data.popleft()
+      rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(data[1], data[0], depth_scale, depth_trunc, False)
+      tsdf_volume.integrate(rgbd, intrinsics, np.linalg.inv(data[2]))
+      reconstructed_frame_count += 1
+      if reconstructed_frame_count % 150 == 0:
+          mesh = tsdf_volume.extract_triangle_mesh()
+          mesh_msg = meshToRos(mesh)
+          mesh_msg.header.stamp = rospy.get_rostime()
+          mesh_msg.header.frame_id = relative_frame
+          mesh_pub.publish(mesh_msg)
+  if integrating_queue:
+    print("Integration done")
+    integration_done = True
 
 def main():
-  global camera_info_topic, tf_listener, tracking_frame, world_frame
+  global camera_info_topic, tf_listener, tracking_frame, world_frame, mesh_pub
 
   rospy.init_node('open3d_tsdf_rgb_recorder', anonymous=True)
 
@@ -203,6 +260,10 @@ def main():
   color_sub = Subscriber(color_image_topic, Image)
   tss = ApproximateTimeSynchronizer([depth_sub, color_sub], cache_count, slop, allow_headerless)
   tss.registerCallback(cameraCallback)
+
+  rospy.Timer(rospy.Duration(0.5), timerReconstruction)
+
+  mesh_pub = rospy.Publisher("open3d_mesh", Marker, queue_size=10)
 
   start_server = rospy.Service('start_reconstruction', StartYakReconstruction, startYakReconstructionCallback)
   stop_server = rospy.Service('stop_reconstruction', StopYakReconstruction, stopYakReconstructionCallback)
